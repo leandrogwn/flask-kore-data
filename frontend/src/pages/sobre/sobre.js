369262f6d57d18fecf43afaabe748b8a886ad87c
Leandro Gonçalves <leandro.gwn@gmail.com>
2024-06-26 12:35:33 -0300

    feat: Tela sobre e imagem logo.

---
+++ frontend/src/pages/sobre/sobre.js
@@ -0,0 +1,20 @@
+import Backend from "./backend";
+import Frontend from "./frontend";
+
+function Sobre() {
+    return (
+        <div>
+            <h4> Teste com implementação de Api utilizando Python e Postgres com front em React.</h4>
+
+
+            <p>Stack utilizada</p>
+
+            <Backend />
+            <Frontend />
+
+
+        </div>
+    )
+}
+
+export default Sobre;
